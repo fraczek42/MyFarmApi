df52e2b65123fe1f15532fe98a412f5d4dcfa08c
Hubert Frączkowski <hubertfraczkowski@MacBook-Pro-Hubert.local>
2023-11-12 22:38:09 +0100

    Poprawa projektu

--- MyFarmApi/Controllers/OrderController.cs
+++ MyFarmApi/Controllers/OrderController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using MyFarmApi.Models;
 
 namespace MyFarmApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/orders")]
     public class OrderController : ControllerBase
     {
         private readonly ILogger<OrderController> _logger;
@@ -14,8 +15,9 @@ namespace MyFarmApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetOrders")]
-        public ActionResult<IEnumerable<Order>> Get()
+        // GET /api/orders
+        [HttpGet]
+        public ActionResult<IEnumerable<Order>> GetAllOrders()
         {
             try
             {
@@ -30,30 +32,152 @@ namespace MyFarmApi.Controllers
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error while fetching orders from the database.");
+                _logger.LogError(ex, "Error while fetching orders.");
                 return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred while processing your request.");
             }
         }
 
+        // GET /api/orders/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Order> GetOrderById(int id)
+        {
+            try
+            {
+                var order = GetSampleOrders().FirstOrDefault(o => o.Id == id);
+
+                if (order == null)
+                {
+                    return NotFound($"Order with ID {id} not found.");
+                }
+
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while fetching order with ID {id}.");
+                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        // POST /api/orders
+        [HttpPost]
+        public ActionResult<Order> CreateOrder([FromBody] Order newOrder)
+        {
+            try
+            {
+                // Logika dodawania nowego zamówienia
+                // ...
+
+                return CreatedAtAction(nameof(GetOrderById), new { id = newOrder.Id }, newOrder);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while creating a new order.");
+                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        // PUT /api/orders/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateOrder(int id, [FromBody] Order order)
+        {
+            try
+            {
+                if (id != order.Id)
+                {
+                    return BadRequest("Order ID mismatch.");
+                }
+
+                // Logika aktualizacji zamówienia
+                // ...
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while updating order with ID {id}.");
+                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred while processing your request.");
+            }
+        }
+
+        // DELETE /api/orders/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteOrder(int id)
+        {
+            try
+            {
+                // Logika usuwania zamówienia
+                // ...
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while deleting order with ID {id}.");
+                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred while processing your request.");
+            }
+        }
 
         private List<Order> GetSampleOrders()
         {
             return new List<Order>
             {
-                new Order { OrderingPersonId = 1, Quantity = 5, DateOrder = DateTime.Now.AddDays(-5) }
+                new Order
+                {
+                    Id = 1,
+                    OrderingPersonId = 1,
+                    Quantity = 5,
+                    DateOrder = DateTime.Now.AddDays(-5),
+                    AmountPerQuantity = 40,
+                    QuantityPerPackage = 25,
+                    OrderedGoodsId = 1,
+                    Status = 1
+                },
+                new Order
+                {
+                    Id = 2,
+                    OrderingPersonId = 2,
+                    Quantity = 30,
+                    DateOrder = DateTime.Now.AddDays(-10),
+                    AmountPerQuantity = 35,
+                    QuantityPerPackage = 20,
+                    OrderedGoodsId = 2,
+                    Status = 2
+                },
+                new Order
+                {
+                    Id = 3,
+                    OrderingPersonId = 3,
+                    Quantity = 22,
+                    DateOrder = DateTime.Now.AddDays(-3),
+                    AmountPerQuantity = 45,
+                    QuantityPerPackage = 30,
+                    OrderedGoodsId = 3,
+                    Status = 1
+                },
+                new Order
+                {
+                    Id = 4,
+                    OrderingPersonId = 2,
+                    Quantity = 30,
+                    DateOrder = DateTime.Now.AddDays(-7),
+                    AmountPerQuantity = 50,
+                    QuantityPerPackage = 10,
+                    OrderedGoodsId = 4,
+                    Status = 3
+                },
+                new Order
+                {
+                    Id = 5,
+                    OrderingPersonId = 3,
+                    Quantity = 30,
+                    DateOrder = DateTime.Now.AddDays(-1),
+                    AmountPerQuantity = 60,
+                    QuantityPerPackage = 5,
+                    OrderedGoodsId = 5,
+                    Status = 2
+                }
             };
         }
     }
-}
-
-public class Order
-{
-    public int Id { get; set; }
-    public int OrderingPersonId { get; set; }
-    public int Quantity { get; set; }
-    public int QuantityPerPackage { get; set; }
-    public int OrderedGoodsId { get; set; }
-    public decimal AmountPerQuantity { get; set; }
-    public DateTime DateOrder { get; set; }
-    public int Status { get; set; }
 }
---
+++ MyFarmApi/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MyFarmApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int OrderingPersonId { get; set; }
+        public int Quantity { get; set; }
+        public int QuantityPerPackage { get; set; }
+        public int OrderedGoodsId { get; set; }
+        public decimal AmountPerQuantity { get; set; }
+        public DateTime DateOrder { get; set; }
+        public int Status { get; set; }
+    }
+}
+
